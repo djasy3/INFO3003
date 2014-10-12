44ba3074be2cfbbecafe430ef0d2d9eebbf0ce67
djasy3 <djasy3@gmail.com>
2014-10-12 17:02:39 -0300

    devoir
    version 1.0

---
+++ devoirinfo3003/devoirinfo3003/devoirinfo3003.cpp
@@ -0,0 +1,11 @@
+// devoirinfo3003.cpp : définit le point d'entrée pour l'application console.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "bonjour le monde " << endl;
+	return 0;
+}
