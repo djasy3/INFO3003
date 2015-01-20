afe467aeaa01d24763eb3f9f99d3b6c34ce2248a
djasy3 <djasy3@gmail.com>
2015-01-19 20:50:30 -0400

    Update parsing.h

--- devoirinfo3003/devoirinfo3003/parsing.h
+++ devoirinfo3003/devoirinfo3003/parsing.h
@@ -2,12 +2,24 @@
 #define PARSING_H
 
 #include <vector>
+#include <cctype>
 #include <string>
 #include <iostream>
+#include <iomanip>
+#include <fstream>
 #include <map>
+#include <bitset>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
+typedef struct ActivationBit ActivationBit;
+struct ActivationBit
+{
+	vector<int> position;
+	vector<int> bitActivE;
+};
 //cette structure va contenir notre table d'opcode
 typedef struct TableOpcode TableOpcode;
 struct TableOpcode
@@ -15,29 +27,43 @@ struct TableOpcode
 	string m_adresse;
 	string m_opcode;
 };
-
-typedef struct Instruction Instruction;
-struct Instruction
+//
+typedef struct VariablesBooleenes VariablesBooleenes;
+struct VariablesBooleenes
 {
-	string m_micro;
-	string m_signal;
+	string nomVar;//nom de la variable booleennes
+	string adrsSel;//adresse de la variable booleenes(mux)
+	string adrsBr;//adresse du branchement
 };
 
-vector<Instruction>traitementInstructions(vector<string>);
-
-string ignoreComments(string);	//mÈthode qui permet d'ignorer les commentaires dans le code
-vector<string>split(string, char[]);	//mÈthode qui permet d'obtenir chaque mot pour faire le parsing!
+map<string, string>traitementInstructions(vector<string>);
+string ignoreComments(string);	//m√©thode qui permet d'ignorer les commentaires dans le code
+vector<string> split(string, char[]);	//m√©thode qui permet d'obtenir chaque mot pour faire le parsing!
+vector<string> split(vector<string>, char []);
 vector<string>motsEnMiniscules(vector<string>);
-vector<string> getAdrsLabels(vector<string>);	//mÈthode qui retourne les adresses des labels rencontrÈs
-string getAdrsLabel(map<string, string>, string);//va rÈcupÈrer une adresse spÈcifique du label dans le tableau des labels
-bool isLabel(string);			//mÈthode qui vÈrifie si un mot est un label
-bool isOpcode(string);			//mÈthode qui vÈrifie si un mot est un opcode
+vector<string> extractionInstructions();//permet d'extraire les microinstructions
+vector<string> getAdrsLabels(vector<string>);	//m√©thode qui retourne les adresses des labels rencontr√©s
+string getAdrsLabel(map<string, string>, string);//va r√©cup√©rer une adresse sp√©cifique du label dans le tableau des labels
+bool isLabel(string);			//m√©thode qui v√©rifie si un mot est un label
+bool isOpcode(string);			//m√©thode qui v√©rifie si un mot est un opcode
 bool isRegister(string);
 bool isBranch(string);
+bool isOpLogique(string);
 bool isBus(string);
-string intTobin(int);		//fonction qui convertit les entiers en binaire sous forme des chaines de caractËres
+string intTobinL(int);		//fonction qui convertit les entiers en binaire sous forme des chaines de caract√®res
+string intTobinVB(int);
 int positionDeuxPoint(string);
 int positionPointExclamation(string);
+int getMax(vector<TableOpcode>); //fonction qui renvoi le plus grand √©l√©ment d'un tableau
 bool estIndependante(string);
+string trim(string, char[]);
+string trim(string);
+vector<int> ConditionBranch(string);
+map<string, string> MapJeuInstruction (vector<string>);
+vector<TableOpcode> JeuxInstructions(vector<string>);
+string signal_F(string);
+string Bit_Active(string);
+int positionEgal(string);
+string couper1erMot(string);
 
 #endif
